41ba0315e64d3d8bda199ccfb59fe02abc4cb1d7
Connor Shore <cjshore12@gmail.com>
2025-09-03 19:53:32 -0600

    Run pipelines in an isolated directory

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -8,41 +8,65 @@ import (
 	"github.com/ConnorShore/micro-ci/internal/pipeline"
 )
 
-type Runner struct{}
+type Runner struct {
+	executionDir    string
+	environmentVars []string
+}
 
 func NewRunner() (*Runner, error) {
 	return &Runner{}, nil
 }
 
+// TODO: Write decorators for logging
+
 // Runs the passed in pipeline
 func (r *Runner) Run(p pipeline.Pipeline) error {
+	dir, err := os.MkdirTemp("", "microci-runner-env")
+	fmt.Println("Runner dir: ", dir)
+	if err != nil {
+		return fmt.Errorf("failed to create micro-ci runner environment: %v", err)
+	}
+	defer os.RemoveAll(dir)
+
+	r.executionDir = dir
 
 	// get environment variables from pipeline
-	envVars := parseVariables(p.Variables)
+	r.environmentVars = parseVariables(p.Variables)
 
+	fmt.Println("Running pipeline in directory: ", dir)
 	// Run each step in the pipeline
 	for _, step := range p.Steps {
-		run, err := canRunStep(step)
-		if err != nil {
-			fmt.Printf("Step [%v] condition failed to parse. Skipping step.\n", step.Name)
-			continue
-		}
-
-		if !run {
-			fmt.Printf("Skipping step [%v].\n", step.Name)
-			continue
-		}
-
-		localVars := parseVariables(step.Variables)
-
-		fmt.Printf("===== Executing Step [%v] =====\n", step.Name)
-		err = executeScript(step.Script, append(envVars, localVars...))
+		r.RunStep(step)
 		if err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func (r *Runner) RunStep(s pipeline.Step) (bool, error) {
+	run, err := canRunStep(s)
+	if err != nil {
+		fmt.Printf("Step [%v] condition failed to parse. Skipping step.\n", s.Name)
+		return false, err
+	}
+
+	if !run {
+		fmt.Printf("Skipping step [%v].\n", s.Name)
+		return false, nil
+	}
+
+	// Get the local variables to the step to append to the environment vars for this step
+	localVars := parseVariables(s.Variables)
+
+	fmt.Printf("===== Executing Step [%v] =====\n", s.Name)
+	err = executeScript(s.Script, r.executionDir, append(r.environmentVars, localVars...))
+	if err != nil {
+		return false, fmt.Errorf("failed to execute script for step [%v]: %v", s.Name, err)
+	}
+
+	return true, nil
 }
 
 // Evaluates Step's condition to determine if step should be ran
@@ -61,10 +85,11 @@ func parseVariables(vars map[string]string) []string {
 }
 
 // Executes the script for a step
-func executeScript(script pipeline.Script, variables []string) error {
+func executeScript(script pipeline.Script, directory string, variables []string) error {
 	cmd := exec.Command("sh", "-c", string(script))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Dir = directory
 	cmd.Env = variables
 
 	err := cmd.Run()
